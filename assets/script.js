faca1e74a641d373f2e595314a9b1d3909a6d594
Shannon Quinn <shannon.quinn91@gmail.com>
2020-11-03 12:57:01 -0500

    Online ordering page

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,38 @@ function getNutrientInfo(dessertName) {
         //console.log("test")
 getNutrientInfo("cakes")
     });
+
+//for menu page
+
+$("#add-to-cart").on("click", function(){
+  console.log("buttons work")
+})
+
+//Copied from MDBootsrap eCommerce
+// $('#add-to-cart').on('click', (e) => {
+//   //addToCart(e.currentTarget)
+//   console.log("buttons work");
+// })
+
+// const addToCart = (product) => {
+//   const productId = $(product).attr('productId');
+//   const isAlreadyInCart = $.grep(productsInCart, el => {return el.id == productId}).length;
+
+//   if (isAlreadyInCart) {
+//     $.each(storageData, (i, el) => {
+//       if (productId == el.id) {
+//         el.itemsNumber += 1;
+//       }
+//     })
+//   } else {
+//     const newProduct = {
+//       id: Number(productId),
+//       itemsNumber: 1
+//     }
+
+//     storageData.push(newProduct);
+//   }
+
+//   updateCart();
+//   updateProductList();
+// }
