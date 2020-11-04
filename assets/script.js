e1c7600da44af73389c319c2cffcd3b06c925a09
Shannon Quinn <shannon.quinn91@gmail.com>
2020-11-04 10:33:49 -0500

    Updates to cart page

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
- Menu-Page
+
 // Menu Api JS
     $(document).ready(function() {
         
@@ -45,7 +45,7 @@
         return nutrientEl;
       }
     });
-=======
+
 function getNutrientInfo(dessertName) {
     const queryURL = "https://trackapi.nutritionix.com/v2/natural/nutrients"
     const sendQueryData = {
@@ -66,32 +66,11 @@ function getNutrientInfo(dessertName) {
 getNutrientInfo("cakes")
     });
 
+//Cart JS    
+var test = $("#cake-one-q").text()
 
+$(".update").on("click", function(){
+  console.log("works")
+  console.log(test)
+})
 
-
-<<<<<<< HEAD
-=======
-// const addToCart = (product) => {
-//   const productId = $(product).attr('productId');
-//   const isAlreadyInCart = $.grep(productsInCart, el => {return el.id == productId}).length;
-
-//   if (isAlreadyInCart) {
-//     $.each(storageData, (i, el) => {
-//       if (productId == el.id) {
-//         el.itemsNumber += 1;
-//       }
-//     })
-//   } else {
-//     const newProduct = {
-//       id: Number(productId),
-//       itemsNumber: 1
-//     }
-
-//     storageData.push(newProduct);
-//   }
-
-//   updateCart();
-//   updateProductList();
-// }
-main
->>>>>>> fbcfee4a726a27b8e219eee986d433aac41a7d2b
